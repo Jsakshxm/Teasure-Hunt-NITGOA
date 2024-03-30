16f6d51692a2f50e0385e85c174e97d4b2c98c61
Saksham jain <130843255+Jsakshxm@users.noreply.github.com>
2024-03-30 16:44:23 +0530

    start page image

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,9 @@ import Link from "next/link";
 const Page = () => {
   return (
     <div className='flex flex-col items-center justify-center min-h-screen p-4 text-white bg-black'>
+    <img src="/logo.png" alt="" width="200" />
       <h1 className='text-2xl font-extrabold text-center md:text-4xl'>Canary-1961</h1>
+      
       <Link href="Trial"><button className='p-2 mt-4 font-semibold transition duration-300 ease-out border border-purple-700 w-44 sm:w-auto sm:px-10 hover:bg-purple-700 hover:text-white'> 
         Unlock
       </button> </Link>
